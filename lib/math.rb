6c6ac174e2be46b1726e8bd9d4dff3e1fa7adab2
optimistic-creator-7568 <alexandro.alanya@gmail.com>
2019-05-19 19:26:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,13 @@
 def addition(num1, num2)
-  put 1+2
+  1+2
 end
 
 def subtraction(num1, num2)
-  put 1-2
+  1-2
 end
 
 def division(num1, num2)
-  put 1/2
+  1/2
 end
 
 def multiplication(num1, num2)
